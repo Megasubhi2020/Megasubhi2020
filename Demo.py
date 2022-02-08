23c504035085b340911619571ce334c22e7b75b3
Megasubhi2020 <99231528+Megasubhi2020@users.noreply.github.com>
2022-02-07 22:42:25 -0500

    Create Demo.py

---
+++ Demo.py
@@ -0,0 +1,10 @@
+Hi, My name is Malaak 
+Hello, World
+My favorite book is ''The Fault in Our Stars''
+Sir Allen MacNab Secondary School Hwdsb.on.ca 
+My favorite music is Rthym and blues
+My favorite tv show is Iclary
+My favorite movie is Yes Day
+My favorite color is blue
+I am a very shy girl
+I am a very picky reader
